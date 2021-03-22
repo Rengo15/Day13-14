34f41b9eed3fbcbe9c189533f4ed12a59cab3fb2
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-22 01:22:35 +0100

    Created ex01

---
+++ ex01/scripts/index.js
@@ -0,0 +1,75 @@
+var modal = document.getElementById("modal1");
+var frame1 = document.getElementById("frame1");
+var black = document.getElementsByClassName("black");
+var close = document.getElementById("close");
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+var img5 = document.getElementById("img5");
+var img6 = document.getElementById("img6");
+var img7 = document.getElementById("img7");
+var img8 = document.getElementById("img8");
+var img9 = document.getElementById("img9");
+var img10 = document.getElementById("img10");
+
+img1.onclick = function () {
+
+  modal1.style.display = "block";
+  frame1.src = "image01.html";
+};
+img2.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image02.html";
+};
+img3.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image03.html";
+};
+img4.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image04.html";
+};
+img5.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image05.html";
+};
+img6.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image06.html";
+};
+img7.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image07.html";
+};
+img8.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image08.html";
+};
+img9.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image09.html";
+};
+img10.onclick = function () {
+
+  modal.style.display = "block";
+  frame1.src = "image10.html";
+};
+
+modal.onclick = function () {
+
+  modal.style.display = "none";
+};
+
+close.onclick = function () {
+  
+  modal.style.display = "none";
+};
